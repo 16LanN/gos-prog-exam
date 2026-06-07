0241065cba27c097c57ae7bb59826ba4ff44f51c
LanN <lannsilbay@gmail.com>
2026-06-07 19:04:48 +0600

    Basic Array Operations

--- Basic Array Operations/task_4.py
+++ Basic Array Operations/task_4.py
@@ -1,15 +1,11 @@
 # 4.	Find the minimum and maximum elements in an array and swap their positions.
 arr = [42, -4, 2, 6, 8, -9, -12, 5, -3, 7]
-mi = [0, False]
-ma = [0, False]
+mi = 0
+ma = 0
 for i in range(len(arr)):
     if arr[i] == min(arr):
-        mi[0] = i
-        mi[1] = True
+        mi = i
     if arr[i] == max(arr):
-        ma[0] = i
-        ma[1] = True
-    if ma[1] and mi[1]:
-        break
-arr[mi[0]], arr[ma[0]] = arr[ma[0]], arr[mi[0]]
+        ma = i
+arr[mi], arr[ma] = arr[ma], arr[mi]
 print(arr)
